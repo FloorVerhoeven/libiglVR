14919f842b73870402a2e60bbacfbe3c031b40ea
FloorVerhoeven <floor.verhoeven@home.nl>
2018-11-07 15:46:05 +0100

    Implemented mipmaps for textures in libigl to allow for textures with horizontal lines in VR (at all)

--- include/igl/opengl/MeshGL.cpp
+++ include/igl/opengl/MeshGL.cpp
@@ -111,11 +111,13 @@ IGL_INLINE void igl::opengl::MeshGL::bind_mesh()
 
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, vbo_tex);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
   if (dirty & MeshGL::DIRTY_TEXTURE)
   {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_u, tex_v, 0, GL_RGBA, GL_UNSIGNED_BYTE, tex.data());
